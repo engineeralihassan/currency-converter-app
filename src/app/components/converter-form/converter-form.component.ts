f8e8e75f81248f1f64e0a89cc80118573e55bb28
Ali Hassan <engineralihassan@gmail.com>
2023-12-04 07:21:32 -0500

    The tas is completed

--- src/app/components/converter-form/converter-form.component.ts
+++ src/app/components/converter-form/converter-form.component.ts
@@ -7,15 +7,14 @@ import { CurrencyService } from 'src/app/currency.service';
 })
 export class ConverterFormComponent {
   currencies: string[] = [];
-  fromCurrency: string = 'PKR';
-  toCurrency: string = 'USD';
+  fromCurrency: string = 'USD';
+  toCurrency: string = 'PKR';
   amount: number = 1;
   result: number | undefined;
 
   constructor(private currencyService: CurrencyService) {}
 
   ngOnInit(): void {
-    // Fetch the list of currencies when the component initializes
     this.currencyService.getExchangeRates('USD').subscribe((data) => {
       console.log("The data is");
       this.currencies = Object.keys(data.rates);
@@ -31,35 +30,4 @@ export class ConverterFormComponent {
         this.result = this.amount * rate;
       });
   }
-//   currencies: any[] = [];
-//   fromCurrency: any;
-//   toCurrency: any;
-//   amount: number = 1;
-//   result: number | undefined;
-
-//   constructor(private currencyService: CurrencyService) {}
-
-//   ngOnInit(): void {
-//     this.currencyService.getCountries().subscribe((data) => {
-//       console.log("The currencies are:",data);
-//       this.currencies = data
-//         .filter((country: any) => country.currencies && country.currencies[0] && country.currencies[0].code)
-//         .map((country: any) => {
-//           return {
-//             code: country.currencies[0].code,
-//             name: country.name.common,
-//             flag: country.flags[0],
-//           };
-//         });
-//     });
-//   }
-
-//   convertCurrency() {
-//     this.currencyService
-//       .getExchangeRates(this.fromCurrency.code)
-//       .subscribe((data) => {
-//         const rate = data.rates[this.toCurrency.code];
-//         this.result = this.amount * rate;
-//       });
-//   }
- }
+   }
